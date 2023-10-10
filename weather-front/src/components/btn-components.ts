fca045bf8b1b6c90c542ec9f49163353b52921d8
v-alex-dev <vensalex1991@gmail.com>
2023-10-10 14:26:13 +0200

    learn & add component class button

---
+++ weather-front/src/components/btn-components.ts
@@ -0,0 +1,33 @@
+class BtnComponents {
+    private  id: string | undefined;
+    private btnType: "button" | "submit" | "reset" | undefined;
+    private className: string |undefined;
+
+    constructor(id?:string, btnType?:"button" | "submit" | "reset", className?:string) {
+        this.id = id;
+        this.btnType = btnType;
+        this.className = className;
+    }
+
+    initButton(): HTMLButtonElement{
+        const button = document.createElement('button');
+        if (this.id) button.id = this.id;
+        if (this.btnType) button.type = this.btnType;
+        if (this.className) button.className = this.className;
+
+        button.classList.add('bg-blue-500', 'text-white', 'p-2', 'rounded-lg');
+
+        return button
+    }
+
+    initStyle(classList:[string]) : HTMLButtonElement {
+        const button = this.initButton()
+
+        if (classList && classList.length > 0) {
+            button.classList.add(...classList);
+        }
+
+        return button;
+    }
+
+}
