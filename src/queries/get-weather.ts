d6c68022fa1e995619ef95a27aba7974b042806e
Alexandre <vensalex1991@gmail.com>
2023-10-12 16:32:03 +0200

    fix bug

--- src/queries/get-weather.ts
+++ src/queries/get-weather.ts
@@ -1,7 +1,7 @@
-import {keyApi} from "../../keyApi";
+//import {keyApi} from "../../keyApi";
 
 export async function getWeather (value:string): Promise<object> {
-    const key = keyApi;
+    const key = '2768156f77daf3bc47e7d2980448ea31';
     return await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${value}&appid=${key}`)
         .then(data => data.json())
 }
